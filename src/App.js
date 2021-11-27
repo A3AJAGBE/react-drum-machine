6eab5de2a34d13e35724b870433d4c12c991d8cf
Aminat Ajagbe <ajagbeaminat95@gmail.com>
2021-11-27 09:59:08 +0000

    Add header section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Container from 'react-bootstrap/Container';
 
 class App extends React.Component {
  
 	render() {
 		return (
-			<h1>Hello World</h1>
+			<Container fluid id="App">
+				<header className="py-3 mb-4 border-bottom">
+					<span className="fs-3 fw-bold">A3AJAGBE Drum Machine</span>
+				</header>
+				
+				<h1>Wow</h1>
+			</Container>
 		);
 	}
 }
