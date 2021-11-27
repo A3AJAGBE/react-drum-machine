a048d45a291de447d67efae7c6a2c24abaf72119
Aminat Ajagbe <ajagbeaminat95@gmail.com>
2021-11-27 09:32:27 +0000

    Add react-bootstrap installation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends React.Component {
  
-  render() {
-    return (
-      <h1>Hello World</h1>
-    );
-  }
+	render() {
+		return (
+			<h1>Hello World</h1>
+		);
+	}
 }
 
 export default App;
