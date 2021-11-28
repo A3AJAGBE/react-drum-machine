103801613670ced5802ea198f8897a67f28d011c
Aminat Ajagbe <ajagbeaminat95@gmail.com>
2021-11-28 18:37:50 +0000

    Add Card as display

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Button from 'react-bootstrap/Button'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 class App extends React.Component {
  
@@ -17,22 +18,29 @@ class App extends React.Component {
 				</header>
 
 				<main>
-					<Container>
+					<Container id="drum-machine">
 						<Row className="p-3 mb-5 rounded" id="Board-Row">
-							<Col md={6} className="">
-								<Button variant="secondary" size="lg">Q</Button>
-								<Button variant="secondary" size="lg">W</Button>
-								<Button variant="secondary" size="lg">E</Button>
+							<Col md={6} className="Board-Grid" id="Board-Grid">
+								<Button variant="secondary" size="lg" className="drum-pad">Q</Button>
+								<Button variant="secondary" size="lg" className="drum-pad">W</Button>
+								<Button variant="secondary" size="lg" className="drum-pad">E</Button>
 
-								<Button variant="secondary" size="lg">A</Button>
-								<Button variant="secondary" size="lg">S</Button>
-								<Button variant="secondary" size="lg">D</Button>
+								<Button variant="secondary" size="lg" className="drum-pad">A</Button>
+								<Button variant="secondary" size="lg" className="drum-pad">S</Button>
+								<Button variant="secondary" size="lg" className="drum-pad">D</Button>
 
-								<Button variant="secondary" size="lg">Z</Button>
-								<Button variant="secondary" size="lg">X</Button>
-								<Button variant="secondary" size="lg">C</Button>
+								<Button variant="secondary" size="lg" className="drum-pad">Z</Button>
+								<Button variant="secondary" size="lg" className="drum-pad">X</Button>
+								<Button variant="secondary" size="lg" className="drum-pad">C</Button>
 							</Col>
-							<Col md={6} className="mx-auto">min 2</Col>
+							<Col md={6} className="mx-auto">
+							<Card bg="secondary" id="display">
+								<Card.Header as="h3">Dashboard </Card.Header>
+								<Card.Body>
+									<Card.Title>Main Display</Card.Title>
+								</Card.Body>
+							</Card>
+							</Col>
 						</Row>
 					</Container>
 				</main>
