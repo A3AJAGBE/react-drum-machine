172a856c15b1801c727328105700c47117795382
Aminat Ajagbe <ajagbeaminat95@gmail.com>
2021-11-29 17:51:08 +0000

    Update sound arrays

--- src/App.js
+++ src/App.js
@@ -8,119 +8,118 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 const HeaterKitSounds = [
-	{
-	  keyCode: 81,
-	  keyTrigger: 'Q',
-	  id: 'Heater-1',
-	  url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
-	},
-	{
-	  keyCode: 87,
-	  keyTrigger: 'W',
-	  id: 'Heater-2',
-	  url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
-	},
-	{
-	  keyCode: 69,
-	  keyTrigger: 'E',
-	  id: 'Heater-3',
-	  url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
-	},
-	{
-	  keyCode: 65,
-	  keyTrigger: 'A',
-	  id: 'Heater-4',
-	  url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
-	},
-	{
-	  keyCode: 83,
-	  keyTrigger: 'S',
-	  id: 'Clap',
-	  url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
-	},
-	{
-	  keyCode: 68,
-	  keyTrigger: 'D',
-	  id: 'Kick',
-	  url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
-	},
-	{
-	  keyCode: 90,
-	  keyTrigger: 'Z',
-	  id: 'Open-HH',
-	  url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
-	  
-	},
-	{
-	  keyCode: 88,
-	  keyTrigger: 'X',
-	  id: 'Closed-HH',
-	  url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
-	},
-	{
-	  keyCode: 67,
-	  keyTrigger: 'C',
-	  id: "Kick-n'-Hat",
-	  url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
-	}
+  {
+    keyCode: 81,
+    keypress: "Q",
+    id: "Heater-1",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+  },
+  {
+    keyCode: 87,
+    keypress: "W",
+    id: "Heater-2",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+  },
+  {
+    keyCode: 69,
+    keypress: "E",
+    id: "Heater-3",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+  },
+  {
+    keyCode: 65,
+    keypress: "A",
+    id: "Heater-4",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+  },
+  {
+    keyCode: 83,
+    keypress: "S",
+    id: "Clap",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+  },
+  {
+    keyCode: 68,
+    keypress: "D",
+    id: "Kick",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+  },
+  {
+    keyCode: 90,
+    keypress: "Z",
+    id: "Open-HH",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+  },
+  {
+    keyCode: 88,
+    keypress: "X",
+    id: "Closed-HH",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+  },
+  {
+    keyCode: 67,
+    keypress: "C",
+    id: "Kick-n'-Hat",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+  },
 ];
 
 const SmoothPianoSounds = [
-	{
-	  keyCode: 81,
-	  keyTrigger: 'Q',
-	  id: 'Chord-1',
-	  url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3'
-	},
-	{
-	  keyCode: 87,
-	  keyTrigger: 'W',
-	  id: 'Chord-2',
-	  url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3'
-	},
-	{
-	  keyCode: 69,
-	  keyTrigger: 'E',
-	  id: 'Chord-3',
-	  url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3'
-	},
-	{
-	  keyCode: 65,
-	  keyTrigger: 'A',
-	  id: 'Side-Stick',
-	  url: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3'
-	},
-	{
-	  keyCode: 83,
-	  keyTrigger: 'S',
-	  id: 'Shaker',
-	  url: 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3'
-	},
-	{
-	  keyCode: 68,
-	  keyTrigger: 'D',
-	  id: 'Punchy-Kick',
-	  url: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3'
-	},
-	{
-	  keyCode: 90,
-	  keyTrigger: 'Z',
-	  id: 'Open-HH',
-	  url: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3'
-	},
-	{
-	  keyCode: 88,
-	  keyTrigger: 'X',
-	  id: 'Closed-HH',
-	  url: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3'
-	},
-	{
-	  keyCode: 67,
-	  keyTrigger: 'C',
-	  id: 'Snare',
-	  url: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3'
-	}
-  ];
+  {
+    keyCode: 81,
+    keypress: "Q",
+    id: "Chord-1",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3",
+  },
+  {
+    keyCode: 87,
+    keypress: "W",
+    id: "Chord-2",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3",
+  },
+  {
+    keyCode: 69,
+    keypress: "E",
+    id: "Chord-3",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3",
+  },
+  {
+    keyCode: 65,
+    keypress: "A",
+    id: "Side-Stick",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3",
+  },
+  {
+    keyCode: 83,
+    keypress: "S",
+    id: "Shaker",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3",
+  },
+  {
+    keyCode: 68,
+    keypress: "D",
+    id: "Punchy-Kick",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3",
+  },
+  {
+    keyCode: 90,
+    keypress: "Z",
+    id: "Open-HH",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3",
+  },
+  {
+    keyCode: 88,
+    keypress: "X",
+    id: "Closed-HH",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3",
+  },
+  {
+    keyCode: 67,
+    keypress: "C",
+    id: "Snare",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3",
+  },
+];
 
 class DrumPad extends React.Component {
 	render() {
